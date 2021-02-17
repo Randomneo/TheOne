d2f669899462bb9bfe56e362368c2d7ece1a5ea8
Rostyslav Misiura <r.misiura@mobidev.biz>
2021-02-17 12:18:08 +0200

    Basic setup

---
+++ src/the_one/main.py
@@ -0,0 +1,23 @@
+"""
+Main module
+"""
+
+
+import sys
+
+
+def main() -> None:
+    """
+    main entry point
+    """
+    print('hello world')
+
+
+if __name__ == '__main__':
+    sys.exit(main)
+
+
+
+__all__ = [
+    main.__name__,
+]
